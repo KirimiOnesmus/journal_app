cb8002986f7a02f0bc0878ef7bc26df486860dd9
OnesmusKirimi <onesmuskirimi64@gmail.com>
2025-06-20 10:17:45 +0300

    Register Page and responsiveness

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className=' flex items-center min-h-screen bg-linear-to-r from-cyan-400 to-blue-100'>
-            <div className='flex  items-center justify-around md:w-full mx-20 bg-white/30 backdrop-blur-md p-8 rounded-lg shadow-lg'>
+    <div className=' flex items-center min-h-screen bg-linear-to-r from-cyan-400 to-blue-100 w-full'>
+            <div className='md:flex items-center md:justify-around md:w-full md:mx-20 md:bg-white/30 backdrop-blur-none md:backdrop-blur-md p-8 rounded-lg shadow-none md:shadow-lg'>
                     
-                <div className="header flex flex-col gap-6">
+                <div className="header hidden md:flex flex-col gap-6">
                     <h1 className='text-6xl font-bold mb-6'>Welcome Back !</h1>
                     <span className='border-b-4 w-12 mb-6'></span>
                     <p className='text-gray-500 italic text-xl'>"Your journal missed you. Please enter your details."</p>
                 </div> 
-                <form className='flex flex-col items-center justify-center backdrop-blur-md bg-white/30  my-20 p-10 rounded-lg shadow-lg w-full max-w-md'> 
+                <form className='flex flex-col items-center justify-center backdrop-blur-md bg-white/30  md:my-20 p-10 rounded-lg shadow-lg w-full max-w-md'> 
                 
                     <h1 className='mb-8 text-3xl font-bold'>Sign <span className=' border-b-2 '>in</span></h1>
                 <input 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='flex items-center justify-around min-h-screen bg-linear-to-r from-cyan-400 to-blue-100 w-full mx-0'>
+      <div className='md:flex md:items-center md:justify-around w-full md:mx-20 md:bg-white/30 backdrop-blur-none md:backdrop-blur-md p-8 rounded-lg shadow-none md:shadow-lg'>
+        <div className="banner hidden md:flex flex-col mx-0 md:mx-30 gap-5  md:text-left">
+          <p className='uppercase text-2xl font-bold'>join for <span className='text-blue-600'>free</span></p>
+          <h1 className='text-5xl font-semibold'>Start your journey to a clearer mind.</h1>
+          <p className='italic text-xl text-gray-500'>"Your private journal, one entry at a time"</p>
+          <button className='text-left text-blue-500 border max-w-fit py-2 px-4 rounded-full font-medium text-lg cursor-pointer'>Learn More</button>
+        </div>
+        <form action="" className='flex flex-col items-center justify-center  p-10 rounded-lg w-full max-w-md gap-4'>
+          <h1 className='mb-8 text-3xl font-bold text-left'>Create New Account <span className='text-blue-600'>.</span></h1>
+          <input type="text" name="fullName" id="" placeholder='Your Name' className=' w-92 p-2 rounded-lg shadow-lg bg-white/30 outline-none focus:border border-blue-500'/>
+          <input type="text" name="username" id="" placeholder='Username' className='  w-92 p-2 rounded-lg shadow-lg bg-white/30 outline-none focus:border border-blue-500'/>
+          <input type="email" name="email" id="" placeholder='Your Email' className='  w-92 p-2 rounded-lg shadow-lg bg-white/30 outline-none focus:border border-blue-500'/>
+          <input type="password" name="password" id="" placeholder='Password' className=' w-92 p-2 rounded-lg shadow-lg bg-white/30 outline-none focus:border border-blue-500'/>
+          <input type="password" name="confirmPassword" id="" placeholder='Confirm Password' className=' w-92 p-2 rounded-lg shadow-lg bg-white/30 outline-none focus:border border-blue-500'/>
+          
+          <p className='text-right w-full'>Already a member? <a href="/" className='text-blue-500'>Login</a></p>
+          <button type="submit" className=' bg-blue-600 w-92 p-2 rounded-lg text-white text-xl cursor-pointer font-medium'>Create Account</button>
+          
+        </form>
+      </div>
+    </div>
   )
 }
 
