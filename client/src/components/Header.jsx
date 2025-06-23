5d4c6770242638a1de909e1673955cfece4558a4
OnesmusKirimi <onesmuskirimi64@gmail.com>
2025-06-23 14:36:53 +0300

    Complete Landing Page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { FaHome,FaTrafficLight } from "react-icons/fa";
 import { LuBrainCircuit } from "react-icons/lu";
+import { MdMenuBook } from "react-icons/md";
 const Header = () => {
   return (
     <div className='flex justify-between items-center p-4 fixed text-balck shadow-md backdrop-blur-sm w-full bg-transparent'>
         <div className="logo">
-            <a href="#">Daily Journal</a> 
+            <a href="#" className='text-xl md:text-2xl font-bold flex gap-2'>Daily Journal<MdMenuBook className='text-blue-500'/></a> 
         </div>
-        <div className="nav-links flex space-x-14 uppercase">
-            <a href="/" 
-            className='hover:text-blue-500 hover:text-xl hover:font-semibold  flex items-center gap-1 transition duration-500'><FaHome className='text-xl'/>Home</a>
-            <a href="" className='hover:text-blue-500 hover:text-xl hover:font-semibold flex items-center gap-1 transition duration-500'><FaTrafficLight className='text-xl'/>Features</a>
-            <a href="/" className='hover:text-blue-500 hover:text-xl hover:font-semibold flex items-center gap-1 transition duration-500'><LuBrainCircuit className='text-xl'/>Why it Matters</a>
-            
+        <div className="nav hidden md:flex items-center justify-between gap-6">
+            <div className="nav-links flex space-x-14 uppercase">
+              <a href="/" 
+              className='hover:text-blue-500 hover:text-xl hover:font-semibold  flex items-center gap-1 transition duration-500'><FaHome className='text-xl'/>Home</a>
+              <a href="" className='hover:text-blue-500 hover:text-xl hover:font-semibold flex items-center gap-1 transition duration-500'><FaTrafficLight className='text-xl'/>Features</a>
+              <a href="/" className='hover:text-blue-500 hover:text-xl hover:font-semibold flex items-center gap-1 transition duration-500'><LuBrainCircuit className='text-xl'/>Why it Matters</a>
+              
+          </div>
+          <button className='bg-black text-white px-8 py-2 rounded-full  transition duration-200'>
+              <a href="/login" className='text-xl font-bold'>Join Us</a>
+          </button>
         </div>
-        <button className='bg-black text-white px-8 py-2 rounded-full  transition duration-200'>
-            <a href="/login" className='text-xl font-bold'>Join Us</a>
-        </button>
+        
     </div>
   )
 }
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import{Header, Footer} from '../components'
+import{Header} from '../components'
 import Journaling from '../assets/Journaling.jpg'
+import { FaTelegramPlane } from "react-icons/fa";
+import { MdOutlineEmail,MdOutlinePhone, MdWhatsapp,MdAccessTime } from "react-icons/md";
 const features = [
   {
     title: "Daily Journaling",
@@ -36,6 +38,7 @@ const features = [
     icon: "🔒",
   }
 ];
+const today = new Date();
 const Homepage = () => {
   return (
     <div className=' flex flex-col'>
@@ -44,18 +47,18 @@ const Homepage = () => {
       </div>
       <div className='flex justify-between items-center min-h-screen gap-6 w-full'>
         <div className='flex flex-col justify-center items-start  text-center p-8 w-full'>
-          <h2 className='w-full text-5xl px-50 font-bold py-10'>Write it. <span className='text-6xl text-blue-300'>Feel it.</span> Heal it.</h2>
+          <h2 className='w-full text-5xl md:px-50 font-bold py-10'>Write it. <span className='text-6xl text-blue-300'>Feel it.</span> Heal it.</h2>
           <p className='w-full text-2xl py-4'>A private journal with mood tracking and an AI that actually listens you.</p>
           <div className='w-full flex flex-col items-center justify-center'>
                <button 
-                className='bg-blue-400 text-white px-8 py-3 rounded-lg transition duration-200 flex items-center mt-10 cursor-pointer'
+                className='bg-blue-400 text-white px-8 py-3 rounded-lg transition duration-200 flex items-center mt-10 cursor-pointer font-bold hover:text-blue-400 border hover:border-blue-400 hover:bg-white '
                 onClick={() => window.location.href = '/register'}>
                Start Journaling
               </button>
           </div>
  
         </div>
-        <div className='flex flex-col items-center justify-center mt-10  '>
+        <div className='hidden md:flex flex-col items-center justify-center mt-10   '>
           <img src={Journaling} alt="Journaling Illustartion" className='rounded-full mr-20 mt-10 ' />
         </div>
 
@@ -102,9 +105,52 @@ const Homepage = () => {
           </div>
 
         </div>
-        <div>
-          <Footer/>
+        <div id='contact' className='bg-gray-50 py-12 px-4 min-h-screen mb-4 md:mb-0 '>
+          <div className="contact-conttainer  w-full h-screen md:flex  items-center justify-around gap-6 px-20 shadow-sm shadow-blue-300 rounded-lg mt-10">
+            <div className="message-section mb-10 md:mb-0">
+              <h2 
+              className='text-center flex items-center pt-2 md:pt-0 text-3xl md:text-4xl gap-4 font-bold my-4'
+              >Contact Us <FaTelegramPlane className='text-blue-500' /></h2>
+              <p className='text-xl'>Any enquiry feel free to contact us any time. We will get back to you as soon as we can.</p>
+              <form action="" className='flex flex-col  gap-4 mt-6 w-full max-w-md'>
+                <input type="text" name="name" id="name" placeholder='Name'
+                className='p-2 md:backdrop-blur-lg shadow-md bg-white/40 rounded-md outline-0 focus:shadow-lg focus:shadow-blue-200 text-lg'
+                />
+                <input type="email" name="email" id="email" placeholder='Email'
+                className='p-2 md:backdrop-blur-lg shadow-md bg-white/40 rounded-md outline-0 focus:shadow-lg focus:shadow-blue-200 text-lg'
+                 />
+                <textarea name="message" id="message" placeholder='Message'
+                className='p-2 md:backdrop-blur-lg shadow-md bg-white/40 rounded-md outline-0 focus:shadow-lg focus:shadow-blue-200 text-lg'
+                ></textarea>
+                <button 
+                className='text-white bg-blue-400 py-2 uppercase rounded-sm font-bold cursor-pointer hover:bg-white hover:text-blue-400 hover:border border-blue-400 transition duration-500'
+                >Send</button>
+              </form>
+            </div>
+            <div className="contact-info">
+              <div className="card bg-blue-500 text-white p-6 rounded-md shadow-lg ">
+                <h2 className='text-3xl font-bold'>Info</h2>
+                <ul className='mt-4 flex flex-col gap-2 md:gap-8 w-90 h-fit md:h-max'>
+                  <li className='flex items-center gap-4 text-lg font-extralight'> <MdOutlineEmail  className='text-2xl'/> info@dailyjournal.co.ke</li>
+                  <li className='flex items-center gap-4 text-lg font-extralight'> <MdOutlinePhone className='text-2xl' />+254768444502</li>
+                  <li className='flex items-center gap-4 text-lg font-extralight'> <MdWhatsapp className='text-2xl' /> +254768444502</li>
+                  <li className='flex items-center gap-4 text-lg font-extralight'> <MdAccessTime  className='text-2xl'/>09:00 - 17:00</li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+         
           
+        </div>
+        <div id="footer" className=''>
+          <footer className='bg-gray-800 text-white py-4'>
+            <div className='max-w-6xl mx-auto text-center'>
+              <p className='text-sm'>© {today.getFullYear()} Daily Journal. All rights reserved.</p>
+              <p>Built for mindfulness,clarity, and self-reflection.</p>
+              <p className='text-sm'>Made with ❤️ by Onesmus Kirimi</p>
+            </div>
+          </footer>
         </div>
 
     </div>
